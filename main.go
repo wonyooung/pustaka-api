de1a64505b4f2e795d7caab8fd914d73053bb31b
deemisterious <dimasixc22@gmail.com>
2024-07-30 14:33:46 +0700

    param and query
    membuat handler yang dapat menerima param dan query

--- main.go
+++ main.go
@@ -6,10 +6,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+
+
 func main() {
 	router := gin.Default()
 	router.GET("/", rootHandler)
 	router.GET("/hello", helloHandler)
+	router.GET("/books/:id/:title", booksHandler)
+	router.GET("/query", queryHandler)
+
+
 	router.Run(":3000")
 }
 
@@ -22,5 +28,22 @@ func rootHandler(c *gin.Context) {
 func helloHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"content": "hello content",
+	})
+}
+
+func booksHandler(c *gin.Context) {
+	id := c.Param("id")
+	title := c.Param("title")
+	c.JSON(http.StatusOK, gin.H{
+		"id": id,
+		"title" : title,
+	})
+}
+func queryHandler(c *gin.Context) {
+	id := c.Query("id")
+	title := c.Query("title")
+	c.JSON(http.StatusOK, gin.H{
+		"id": id,
+		"title" : title,
 	})
 }
