013783ce916eb7f71d1c9255c5b3c85a723d352f
deemisterious <dimasixc22@gmail.com>
2024-07-30 15:11:27 +0700

    menambahkan method POST

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
-
-
 
 func main() {
 	router := gin.Default()
@@ -14,7 +13,7 @@ func main() {
 	router.GET("/hello", helloHandler)
 	router.GET("/books/:id/:title", booksHandler)
 	router.GET("/query", queryHandler)
-
+	router.POST("/books", postBooksHandler)
 
 	router.Run(":3000")
 }
@@ -35,15 +34,38 @@ func booksHandler(c *gin.Context) {
 	id := c.Param("id")
 	title := c.Param("title")
 	c.JSON(http.StatusOK, gin.H{
-		"id": id,
-		"title" : title,
+		"id":    id,
+		"title": title,
 	})
 }
 func queryHandler(c *gin.Context) {
 	id := c.Query("id")
 	title := c.Query("title")
 	c.JSON(http.StatusOK, gin.H{
-		"id": id,
-		"title" : title,
+		"id":    id,
+		"title": title,
+	})
+}
+
+type BookInput struct {
+	Title    string
+	Price    int
+	Subtitle string `json:sub_title`
+}
+
+func postBooksHandler(c *gin.Context) {
+	// title, price
+	var bookInput BookInput
+
+	err := c.ShouldBindJSON(&bookInput)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"title":    bookInput.Title,
+		"price":    bookInput.Price,
+		"sub_title": bookInput.Subtitle,
 	})
 }
