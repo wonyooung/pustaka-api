92aacf47870f4f0ce70993899d29b9945a0c7bc6
deemisterious <dimasixc22@gmail.com>
2024-07-30 23:24:57 +0700

    custom error dengan validator

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 func main() {
@@ -49,19 +51,23 @@ func queryHandler(c *gin.Context) {
 
 type BookInput struct {
 	Title string `json:"title" binding:"required"`
-	Price int    `json:"price" binding:"required,number"`
+	Price json.Number `json:"price" binding:"required,number"`
 }
 
 func postBooksHandler(c *gin.Context) {
-	// title, price
 	var bookInput BookInput
 
 	err := c.ShouldBindJSON(&bookInput)
 
 	if err != nil {
-		c.JSON(http.StatusBadRequest, err)
-		fmt.Println(err)
-		return
+		
+		for _, e := range err.(validator.ValidationErrors){
+			errorMessage := fmt.Sprintf("Error on field: %s, condition: %s", e.Field(), e.ActualTag())
+			c.JSON(http.StatusBadRequest, errorMessage)
+			return
+		}
+		
+	
 	}
 
 	c.JSON(http.StatusOK, gin.H{
