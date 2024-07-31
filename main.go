d1918e746eaa7ff3ca6a08d3efccdd120f330618
deemisterious <dimasixc22@gmail.com>
2024-07-31 11:16:55 +0700

    feat : install driver mysql
    connect to db mysql , change port from 3000 to 111 because this port already to use

--- main.go
+++ main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"fmt"
+	"log"
 	"pustaka-api/handler"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
-	
+	dsn := "root:@tcp(127.0.0.1:3306)/pustaka-api?charset=utf8mb4&parseTime=True&loc=Local"
+  	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+   
+   if err != nil {
+   		log.Fatal("db connection error")
+   }
+   
+   fmt.Println("Db connect su")
+   
+   
 	router := gin.Default()
 	
 	v1 := router.Group("/v1")
@@ -17,7 +31,7 @@ func main() {
 	v1.GET("/query", handler.QueryHandler)
 	v1.POST("/books", handler.PostBooksHandler)
 	
-	router.Run(":3000")
+	router.Run(":111")
 }
 
 
