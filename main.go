93bf792be37628046aa446bb4bf450f56d932909
deemisterious <dimasixc22@gmail.com>
2024-07-31 09:46:56 +0700

    feat : versioning

--- main.go
+++ main.go
@@ -9,12 +9,17 @@ import (
 )
 
 func main() {
+	
 	router := gin.Default()
-	router.GET("/", rootHandler)
-	router.GET("/hello", helloHandler)
-	router.GET("/books/:id/:title", booksHandler)
-	router.GET("/query", queryHandler)
-	router.POST("/books", postBooksHandler)
+	
+	v1 := router.Group("/v1")
+	
+	
+	v1.GET("/", rootHandler)
+	v1.GET("/hello", helloHandler)
+	v1.GET("/books/:id/:title", booksHandler)
+	v1.GET("/query", queryHandler)
+	v1.POST("/books", postBooksHandler)
 
 	router.Run(":3000")
 }
